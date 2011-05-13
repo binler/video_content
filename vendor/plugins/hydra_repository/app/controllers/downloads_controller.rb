0341f16f917f1bf35f25d83971dad6245e875604
rickjohnson <rick.johnson@nd.edu>
2011-05-13 11:17:54 -0400

    Fixed linked file download for mimetype handling and filename

--- vendor/plugins/hydra_repository/app/controllers/downloads_controller.rb
+++ vendor/plugins/hydra_repository/app/controllers/downloads_controller.rb
@@ -13,13 +13,22 @@ class DownloadsController < ApplicationController
       fedora_object = ActiveFedora::Base.load_instance(params[:asset_id])
       if params[:download_id]
         @datastream = fedora_object.datastreams[params[:download_id]]
-        send_data @datastream.content, :filename=>@datastream.label, :type=>@datastream.attributes["mimeType"]
+        filename = @datastream.label
+        filename = params[:filename] if params[:filename]
+        send_data @datastream.content, :filename=>filename, :type=>@datastream.attributes["mimeType"]
         #send_data( Fedora::Repository.instance.fetch_custom(params[:document_id], "datastreams/#{datastream_id}/content") )
       else
         @datastreams = downloadables( fedora_object )
       end
     end
-    
+
+    # This redirect action is a fallback for the Apache rewrite rule defined in conf/htaccess_[pprd|prod]
+    def redirect
+      dsid = "DS1"
+      dsid = params[:download_id] if params[:download_id]
+      redirect_to "#{Fedora::Repository.instance.base_url}/get/#{params[:pid]}/#{dsid}"
+    end
+
     # def show
     #   puts "params: #{params.inspect}"
     #   puts "Request: #{request.inspect}"
--- vendor/plugins/hydra_repository/lib/hydra/external_assets_helper.rb
+++ vendor/plugins/hydra_repository/lib/hydra/external_assets_helper.rb
@@ -4,7 +4,7 @@ module Hydra::ExternalAssetsHelper
   #
   # @return [FileAsset] the File Asset  
   def create_and_save_external_asset_from_params
-    if params.has_key?(:dsLocation)
+    if params.has_key?(:dsLocation) && params.has_key?(:Filename)
       @new_link = create_external_asset_from_params
       @new_link.save
       return @new_link
@@ -19,12 +19,22 @@ module Hydra::ExternalAssetsHelper
   def create_external_asset_from_params
     attrs={}
     attrs.merge!({:namespace=>params[:namespace]}) if params[:namespace]
-    attrs.merge!(:dsLabel=>params[:label]) if params[:label]   
-    link = ExternalAsset.new(attrs)
-    link.uri = params[:dsLocation]
-    link.uri = "http://#{link.uri}" unless link.uri.include?("://")
-    link.label = params[:label] if params[:label]
-
-    return link
+    attrs.merge!(:dsLabel=>params[:Filename]) if params[:Filename]
+    ext_asset = ExternalAsset.new(attrs)
+    uri = params[:dsLocation]
+    uri = "http://#{uri}" unless uri.include?("://")
+    ext_asset.link_append({:dsLocation=>uri,:dsLabel=>params[:Filename], :mimeType=>mime_type(params[:Filename])})
+    ext_asset.filename = params[:Filename]
+    ext_asset.identifier = uri
+    return ext_asset
   end  
+
+  private
+  # Return the mimeType for a given file name
+  # @param [String] file_name The filename to use to get the mimeType
+  # @return [String] mimeType for filename passed in. Default: application/octet-stream if mimeType cannot be determined
+  def mime_type file_name
+    mime_types = MIME::Types.of(file_name)
+    mime_type = mime_types.empty? ? "application/octet-stream" : mime_types.first.content_type
+  end
 end
