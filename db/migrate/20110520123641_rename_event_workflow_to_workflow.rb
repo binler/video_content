c616ef21f0116e21ac01bf8461ed5fe9c3e1f6d4
Dan Brubaker Horst <dbrubak1@nd.edu>
2011-05-24 12:21:52 -0400

    Further migration refinement

--- db/migrate/20110520123641_rename_event_workflow_to_workflow.rb
+++ db/migrate/20110520123641_rename_event_workflow_to_workflow.rb
@@ -6,11 +6,15 @@ class RenameEventWorkflowToWorkflow < ActiveRecord::Migration
 
     add_column   :workflows, :type, :string, :default => 'EventWorkflow'
     add_index    :workflows, :type
+    add_index    :workflows, :pid
+    add_index    :workflows, :state
   end
 
   # NOTE the type column is necessary for STI to work.
   # Calling down on this migration is destructive.
   def self.down
+    remove_index  :workflows, :pid
+    remove_index  :workflows, :state
     remove_index  :workflows, :type
     remove_column :workflows, :type, :string
     rename_table  :workflows, :event_workflows
